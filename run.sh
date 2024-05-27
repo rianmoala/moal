5a0e941757075afd6898787faab468e5e9c4c046
moala <164861584+rianmoala@users.noreply.github.com>
2024-05-28 05:23:28 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-8fbe162d-438e-439b-a6e6-5a6eb8d5e1c8/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-a83eaf46-be65-4d31-b250-5e60c3677bcb/)
 
 
 echo "$curl_output" >> Update.tsx
